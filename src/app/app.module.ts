33d5dda940be9010d7ca394fbfc63b4f1c221735
ChumaCesibe <chumacesibe@gmail.com>
2022-05-30 08:22:42 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AddProductsComponent } from './cart/add-products/add-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    MenuComponent,
+    AddProductsComponent
   ],
   imports: [
     BrowserModule,
